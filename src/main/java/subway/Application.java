4b28d4dedcf0745b38590e923bd0d7776862a1ab
jaehyeonjung0613 <jaehyeonjung0613@gmail.com>
2025-02-13 14:37:03 +0900

    feat: 15. 애플리케이션 실행

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,10 @@
 package subway;
 
-import java.util.Scanner;
+import subway.presentation.IntroViewController;
 
 public class Application {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        IntroViewController introViewController = new IntroViewController();
+        introViewController.execute();
     }
 }
